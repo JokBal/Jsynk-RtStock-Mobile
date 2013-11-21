3a960480b8ef5b432f4953c5bfb58919a977154c
YukSeungChan <loup1788@gmail.com>
2013-11-21 19:11:21 +0900

    * bug fix. * animation changed.

--- src/org/jokbal/activity/MainActivity.java
+++ src/org/jokbal/activity/MainActivity.java
@@ -2,33 +2,27 @@ package org.jokbal.activity;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.CompoundButton;
 import android.widget.ListView;
-import android.widget.ToggleButton;
+import com.pusher.client.channel.Channel;
 import com.pusher.client.channel.ChannelEventListener;
 import org.jokbal.R;
 import org.jokbal.adapter.ChannelListAdapter;
 import org.jokbal.manager.PusherManager;
-import org.jokbal.model.Channel;
+import org.jokbal.model.JChannel;
 
 import java.util.ArrayList;
 
 public class MainActivity extends Activity implements ChannelEventListener
 {
     private static PusherManager pusherManager = null;
-    private ArrayList<Channel> channelList;
+    private ArrayList<JChannel> channelList;
     private ChannelListAdapter channelListAdapter;
     private ListView channelListView;
-    private static Animation anim;
 
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -42,22 +36,21 @@ public class MainActivity extends Activity implements ChannelEventListener
         pusherManager = PusherManager.getInstance();
         pusherManager.connect(this);
         channelListView = (ListView)findViewById(R.id.lv_channellist);
-        channelList = new ArrayList<Channel>();
+        channelList = new ArrayList<JChannel>();
         channelListAdapter = new ChannelListAdapter(this, R.layout.stocklist_row, channelList);
         channelListView.setAdapter(channelListAdapter);
-        anim = AnimationUtils.loadAnimation(this, R.anim.listview_row_animation);
-        subscribe("KT");
-        subscribe("SK");
-        subscribe("SAMSUNG");
-        subscribe("LG");
-        subscribe("SOMA");
-        subscribe("NIPA");
-        subscribe("CAPTCHA");
-        subscribe("Mac");
-        subscribe("LOL");
-        subscribe("Apple");
-        subscribe("Govern");
-        subscribe("Park");
+        subscribe(new JChannel("KT"));
+        subscribe(new JChannel("SK"));
+        subscribe(new JChannel("SAMSUNG"));
+        subscribe(new JChannel("LG"));
+        subscribe(new JChannel("SOMA"));
+        subscribe(new JChannel("NIPA"));
+        subscribe(new JChannel("CAPTCHA"));
+        subscribe(new JChannel("Mac"));
+        subscribe(new JChannel("LOL"));
+        subscribe(new JChannel("Apple"));
+        subscribe(new JChannel("Govern"));
+        subscribe(new JChannel("Park"));
         channelListAdapter.notifyDataSetChanged();
     }
 
@@ -78,10 +71,6 @@ public class MainActivity extends Activity implements ChannelEventListener
                 startActivity(new Intent(this, JPusherPreferenceActivity.class));
                 return true;
             }
-            case R.id.actionbar_connect:
-            {
-                pusherManager.connect(this);
-            }
             default:
                 return super.onOptionsItemSelected(item);
         }
@@ -96,15 +85,43 @@ public class MainActivity extends Activity implements ChannelEventListener
         }
         return -1;
     }
-
-    public void subscribe(String channelName)
+    private JChannel getChannel(String name)
     {
-        pusherManager.getPuhser().subscribe(channelName, this).bind(channelName, this);
+        for(int i=0; i<channelList.size(); i++)
+        {
+            if(channelList.get(i).getName().equals(name)) return channelList.get(i);
+        }
+        return null;
     }
 
-    public void unsubscribe(String channelName)
+
+    public void subscribe(JChannel jChannel)
     {
-        pusherManager.getPuhser().unsubscribe(channelName);
+        if(jChannel != null && jChannel.isCheckd())
+        {
+            try
+            {
+                pusherManager.getPuhser().subscribe(jChannel.getName(), this).bind(jChannel.getName(), this);
+            }catch (Exception e)
+            {
+
+            }
+        }
+
+    }
+
+    public void unsubscribe(JChannel jChannel)
+    {
+        if(jChannel !=null && !jChannel.isCheckd())
+        {
+            try
+            {
+                pusherManager.getPuhser().unsubscribe(jChannel.getName());
+            }catch (Exception e)
+            {
+
+            }
+        }
     }
 
     @Override
@@ -150,16 +167,15 @@ public class MainActivity extends Activity implements ChannelEventListener
         protected String doInBackground(String... params) {
             String channelName = params[0];
             String data = params[1];
-           int position = getChannelPosition(channelName);
-            if(position == -1)
+            JChannel jChannel = getChannel(channelName);
+            if(jChannel == null)
             {
-                channelList.add(new Channel(data));
+                channelList.add(new JChannel(data));
             }
             else
             {
-                Channel channel = channelList.get(position);
-                channel.setStock(data);
-                channel.setChanged(true);
+                jChannel.setStock(data);
+                jChannel.setChanged(true);
             }
 
             return channelName;
--- src/org/jokbal/adapter/ChannelListAdapter.java
+++ src/org/jokbal/adapter/ChannelListAdapter.java
@@ -2,11 +2,9 @@ package org.jokbal.adapter;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
@@ -14,40 +12,37 @@ import android.widget.TextView;
 import android.widget.ToggleButton;
 import org.jokbal.R;
 import org.jokbal.activity.MainActivity;
-import org.jokbal.manager.PusherManager;
-import org.jokbal.model.Channel;
+import org.jokbal.model.JChannel;
 
 import java.util.ArrayList;
 
-public class ChannelListAdapter extends ArrayAdapter<Channel>
+public class ChannelListAdapter extends ArrayAdapter<JChannel>
 {
     private Context mContext;
     private int mResource;
-    private ArrayList<Channel> channelList;
+    private ArrayList<JChannel> JChannelList;
     private LayoutInflater mInflater;
-    private static PusherManager pusherManager;
 
-    public ChannelListAdapter(Context context, int layoutResource, ArrayList<Channel> channelList)
+    public ChannelListAdapter(Context context, int layoutResource, ArrayList<JChannel> JChannelList)
     {
-        super(context, layoutResource, channelList);
+        super(context, layoutResource, JChannelList);
         this.mContext = context;
         this.mResource = layoutResource;
-        this.channelList = channelList;
+        this.JChannelList = JChannelList;
         this.mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        pusherManager = PusherManager.getInstance();
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent)
     {
-        final Channel channel = channelList.get(position);
+        final JChannel jChannel = JChannelList.get(position);
 
         if(convertView == null)
         {
             convertView = mInflater.inflate(mResource, null);
         }
 
-        if(channel != null)
+        if(jChannel != null)
         {
             TextView channelNameTextView = (TextView) convertView.findViewById(R.id.tv_channel_name);
             TextView priceTextView = (TextView) convertView.findViewById(R.id.tv_price);
@@ -55,50 +50,56 @@ public class ChannelListAdapter extends ArrayAdapter<Channel>
             TextView timeTextView = (TextView) convertView.findViewById(R.id.tv_time);
             ToggleButton channelSwithButton = (ToggleButton) convertView.findViewById(R.id.tb_channel);
 
-            channelNameTextView.setText(channel.getName());
-            priceTextView.setText("Price : " + channel.getStock().getPrice());
-            deltaTextView.setText("Delta : " + channel.getStock().getDelta());
-            timeTextView.setText("Time : " + channel.getStock().getTime());
+            channelNameTextView.setText(jChannel.getName());
+            priceTextView.setText("Price : " + jChannel.getStock().getPrice());
+            deltaTextView.setText("Delta : " + jChannel.getStock().getDelta());
+            timeTextView.setText("Time : " + jChannel.getStock().getTime());
             channelSwithButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
                 public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    if(isChecked)
+                    if (isChecked)
                     {
-                        ((MainActivity)mContext).subscribe(channel.getName());
-                        channel.setCheckd(true);
-                        Log.d("jsynk", "onCheckedChanged true");
+                        jChannel.setCheckd(true);
+                        ((MainActivity) mContext).subscribe(jChannel);
                     }
                     else
                     {
-                        ((MainActivity)mContext).unsubscribe(channel.getName());
-                        channel.setCheckd(false);
-                        Log.d("jsynk", "onCheckedChanged false");
+                        jChannel.setCheckd(false);
+                        ((MainActivity) mContext).unsubscribe(jChannel);
                     }
                 }
             });
-
-            convertView.setTag(channel.getName());
-            if(channel.isCheckd())
+            if(jChannel.isCheckd())
             {
-                if(channel.getStock().getDelta() > 0)
+                if(jChannel.getStock().getDelta() > 0)
                 {
 
                     convertView.setBackgroundColor(Color.argb(255, 46, 203, 113));
+                    if(jChannel.isChanged())
+                    {
+                        convertView.startAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.listview_row_animation_up));
+                    }
                 }
                 else
                 {
                     convertView.setBackgroundColor(Color.argb(255, 231, 76 , 60));
+                    if(jChannel.isChanged())
+                    {
+                        convertView.startAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.listview_row_animation_down));
+                    }
                 }
-                convertView.setAnimation(null);
-                if(channel.isChanged())
+
+                if(jChannel.isChanged())
                 {
-                    convertView.startAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.listview_row_animation));
-                    channel.setChanged(false);
+                    jChannel.setChanged(false);
                 }
+                channelSwithButton.setChecked(true);
             }
             else
             {
                 convertView.setBackgroundColor(Color.argb(255, 236, 240 , 241));
+                convertView.setAnimation(null);
+                channelSwithButton.setChecked(false);
             }
         }
         return convertView;
